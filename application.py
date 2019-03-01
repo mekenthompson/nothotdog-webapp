5d7ba5ef2b6e3f42af4148128be60dece7591a7e
Ken Thompson <me@kenthompson.com.au>
2019-03-01 15:24:37 +1100

    Debug environment vars

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def index():
-    return render_template("upload.html")
+    return render_template("upload.html", PREDICTION_KEY=PREDICTION_KEY, TRAINING_KEY=TRAINING_KEY, PROJECT_NAME=PROJECT_NAME)
 
 @app.route("/upload", methods=['POST'])
 def upload():
